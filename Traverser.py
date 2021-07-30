fe1a428d6d72cc4dbea5dd2e00daf0884ba13da0
benstensen <benstensen@gmail.com>
2021-07-29 23:01:52 -0400

    added comments + usage information

--- Traverser.py
+++ Traverser.py
@@ -10,26 +10,36 @@ eu_title = 'IMSLP-EU Server Main Page'
 ca_title = 'PML-CA Server Main Page'
 titles = [disclaimer_title, subscribe_title, eu_title, ca_title]
 
+# Given an IMSLP link as input, returns the next link in the path to the PDF
+# link, or None if the program speculates it has reached the PDF already.
 def find_next_link(link):
     if link == None: 
         return None
 
+    # You'll see occasional sleeps to respect the servers we're sending GET requests to.
     time.sleep(0.75)
     print('Waiting...')
 
+    # May want to add some code here to handle request timeouts/other errors.
     webpage_response = requests.get(link)
     time.sleep(0.05)
     
+    # Make soup. This can take a really long time if the file returned from the server
+    # is particularly big (e.g. with a large PDF) so it will be necessary to have some
+    # timeout handling for this process.
     link = webpage_response.url
     soup = BeautifulSoup(webpage_response.content, 'lxml')
+    
+    # If soup doesn't have a title, it's probably not an HTML file. Can handle soup-making
+    # issue and this with one check for <!DOCTYPE html>, probably.
     if soup.title == None:
         return None
     title = soup.title.contents[0]
-    # if re.search('\.pdf', link) != None: 
-    #     if not (title in titles): 
-    #         print('unfamiliar title')
-    #         return link
 
+    # If the soup has a title, we check the title against known intermediary page titles to
+    # determine how to construct the next link. 
+    #
+    # If you reach the else at the bottom, PLEASE report the link and title of the page.
     if title == titles[0]:
         # print('landed at disclaimer page...')
         next_extension = soup.select_one('a[href^="/wiki/Special:IMSLPDisclaimerAccept"]').attrs['href']
@@ -50,6 +60,7 @@ def find_next_link(link):
         print('landed at unfamiliar page with title ' + title)
         return None
 
+# Main driver for Traverser functionality.
 def find_pdf_link(starting_link):
     r_link = ''
     print('Attemping to fetch link to file, please wait...')
@@ -60,5 +71,10 @@ def find_pdf_link(starting_link):
     print(r_link)
     return r_link
 
+# Invoke from command line.
 if __name__ == '__main__':
+    if '--help' in sys.argv or len(sys.argv) == 1:
+        print('--help: display this message')
+        print(f'Usage: python {sys.argv[0]} "imslp_link"')
+        exit()
     find_pdf_link(sys.argv[1])
