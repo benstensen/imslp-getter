565468e91e9d101c18102aaeba97cccd63db9eb8
benstensen <benstensen@gmail.com>
2021-07-29 23:06:03 -0400

    added comments + usage information

--- Driver.py
+++ Driver.py
@@ -1,7 +1,9 @@
 import Googler
 import IMSLPer
 import Traverser
+import sys
 
+# Driver for IMSLP Getter
 def imslp_driver():
     search_term = input("Enter search term: ")
     imslp_link = Googler.get_imslp_link_from_search_term(search_term)
@@ -9,5 +11,11 @@ def imslp_driver():
     pdf_link = Traverser.find_pdf_link(file_access_link)
     return pdf_link
 
+# Invoke from command line. Currently relies on keyboard input during execution
+# to fetch final PDF link. 
 if __name__ == '__main__':
+    if '--help' in sys.argv:
+        print('--help: display this message')
+        print(f'Usage: python {sys.argv[0]}')
+        exit()
     imslp_driver()
